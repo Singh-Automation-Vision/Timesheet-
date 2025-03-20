cd6a4c99fe1c8e4594215e4a1af8b8125d5c2f95
sudharshan-n-g <152157391+sudharshan-n-g@users.noreply.github.com>
2025-03-20 19:24:42 +0530

    Project.py

--- Project.py
+++ Project.py
@@ -12,7 +12,7 @@ def retrieve_project():
     client = MongoClient("mongodb+srv://timesheetsystem:SinghAutomation2025@cluster0.alcdn.mongodb.net/")
     db = client["Timesheet"]
     collection = db["Projects"]
-    project_details = collection.find({}, {"project_number": 1, "project_name": 1, "start_date": 1, "end_date": 1, "_id": 0})
+    project_details = collection.find({}, {"projectNumber": 1, "projectName": 1, "startDate": 1, "endDate": 1, "_id": 0})
     project_details_list = list(project_details)
 
     return project_details_list
